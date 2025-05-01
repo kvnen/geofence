4a3d65caa722b2839e3b17a3dc43ae6c07b7f100
kvnen <koivunen.anttisakari@gmail.com>
2025-05-01 19:34:48 +0300

    uart stuff

--- uart.c
+++ uart.c
@@ -4,7 +4,9 @@
 
 
 ISR(USART1_RX_vect){
+while (!(UCSR0A & (1 << UDRE0))){
 	UDR0 = UDR1;		
+}
 }
 
 void initUART1(unsigned short ubrr){	//setup for uart receiving from gps
@@ -17,7 +19,7 @@ void initUART1(unsigned short ubrr){	//setup for uart receiving from gps
 void initUART0(unsigned short ubrr){	//setup for usb serial to pc
 	UBRR0H = (unsigned char)(ubrr>>8);	//set the higher UBRR
 	UBRR0L = (unsigned char) ubrr;		//set the lower UBRR
-	UCSR0B = (1<<TXEN0);			//enable transmit
+	UCSR0B = (1<<TXEN0)|(1<<RXEN1);			//enable transmit
 	UCSR0C = (1<<UCSZ00)|(1<<UCSZ01);	//set message to 8 bits
 }
 
