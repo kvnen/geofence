fcf45dbf91b00e25531b31e25d45d780e9c257ed
Antti Koivunen <85220018+kvnen@users.noreply.github.com>
2025-05-01 19:44:21 +0300

    testing git

--- uart.c
+++ uart.c
@@ -15,7 +15,7 @@ void initUART1(unsigned short ubrr){	//setup for uart receiving from gps
 	UCSR1B = (1<<TXEN1)|(1<<RXEN1)|(1<<RXCIE1);	//enable transmit, receive and receive interrupt
 	UCSR1C = (1<<UCSZ10)|(1<<UCSZ11);	//set message to 8 bits
 }
-
+//for git testing purposes delete later
 void initUART0(unsigned short ubrr){	//setup for usb serial to pc
 	UBRR0H = (unsigned char)(ubrr>>8);	//set the higher UBRR
 	UBRR0L = (unsigned char) ubrr;		//set the lower UBRR
